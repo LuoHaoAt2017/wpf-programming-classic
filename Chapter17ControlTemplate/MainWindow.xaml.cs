9ebe2d6c6826454d99d14d5e6b08007e3bc39c25
洛神赋 <hluo5206@gmail.com>
2023-07-10 20:15:54 +0800

    feat: 列表项被选中

--- Chapter17ControlTemplate/MainWindow.xaml.cs
+++ Chapter17ControlTemplate/MainWindow.xaml.cs
@@ -17,32 +17,53 @@ namespace Chapter17ControlTemplate
 {
 	public partial class MainWindow : Window
 	{
-		private List<MobilePhone> mobilePhones = new List<MobilePhone>()
-		{
-			new MobilePhone() { Name = "小米", Brand = "xiaomi", Price = 1240.0m },
-			new MobilePhone() { Name = "三星", Brand = "samsung", Price = 2800.0m },
-			new MobilePhone() { Name = "苹果", Brand = "apple", Price = 6500 }
-		};
-
 		public MainWindow()
 		{
 			InitializeComponent();
-			Binding binding = new Binding();
-			binding.Source = this.mobilePhones;
-			binding.Path = new PropertyPath(".");
-			binding.Mode = BindingMode.OneWay;
-			this.phoneList.SetBinding(ListBox.ItemsSourceProperty, binding);
+
+			Binding binding1 = new Binding();
+			binding1.Source = new List<MobilePhone>()
+			{
+				new MobilePhone() { Name = "小米", Brand = "xiaomi", Price = 1240.0m },
+				new MobilePhone() { Name = "三星", Brand = "samsung", Price = 2800.0m },
+				new MobilePhone() { Name = "苹果", Brand = "apple", Price = 6500 }
+			};
+			binding1.Path = new PropertyPath(".");
+			binding1.Mode = BindingMode.OneWay;
+			this.phoneList1.SetBinding(ListBox.ItemsSourceProperty, binding1);
 
 			Binding binding2 = new Binding();
 			binding2.Source = new List<MobilePhone>()
-		{
-			new MobilePhone() { Name = "小米", Brand = "xiaomi", Price = 1240.0m },
-			new MobilePhone() { Name = "三星", Brand = "samsung", Price = 2800.0m },
-			new MobilePhone() { Name = "苹果", Brand = "apple", Price = 6500 }
-		};
+			{
+				new MobilePhone() { Name = "小米", Brand = "xiaomi", Price = 1240.0m },
+				new MobilePhone() { Name = "三星", Brand = "samsung", Price = 2800.0m },
+				new MobilePhone() { Name = "苹果", Brand = "apple", Price = 6500 }
+			};
 			binding2.Path = new PropertyPath(".");
 			binding2.Mode = BindingMode.OneWay;
-			this.mobileList.SetBinding(ListBox.ItemsSourceProperty, binding2);
+			this.phoneList2.SetBinding(ListBox.ItemsSourceProperty, binding2);
+
+			Binding binding3 = new Binding();
+			binding3.Source = new List<MobilePhone>()
+			{
+				new MobilePhone() { Name = "小米", Brand = "xiaomi", Price = 1240.0m },
+				new MobilePhone() { Name = "三星", Brand = "samsung", Price = 2800.0m },
+				new MobilePhone() { Name = "苹果", Brand = "apple", Price = 6500 }
+			};
+			binding3.Path = new PropertyPath(".");
+			binding3.Mode = BindingMode.OneWay;
+			this.phoneList3.SetBinding(ListBox.ItemsSourceProperty, binding3);
+
+			Binding binding4 = new Binding();
+			binding4.Source = new List<MobilePhone>()
+			{
+				new MobilePhone() { Name = "小米", Brand = "xiaomi", Price = 1240.0m },
+				new MobilePhone() { Name = "三星", Brand = "samsung", Price = 2800.0m },
+				new MobilePhone() { Name = "苹果", Brand = "apple", Price = 6500 }
+			};
+			binding4.Path = new PropertyPath(".");
+			binding4.Mode = BindingMode.OneWay;
+			this.phoneList4.SetBinding(ListBox.ItemsSourceProperty, binding4);
 		}
 	}
 }
